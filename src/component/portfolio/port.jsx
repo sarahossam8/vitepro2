3f954035703b1bc9c0f0d26d284a019c15427f74
sarahossam8 <hossamsara413@gmail.com>
2024-08-07 12:40:12 +0300

    first commit

--- src/component/portfolio/port.jsx
+++ src/component/portfolio/port.jsx
@@ -2,6 +2,7 @@ import React from "react";
  
 export default function Port(){
 return(
-    <div>port</div>
+    <div className="bg-teal-400 absolute top-20 left-0 right-0 flex justify-center h-[470px] items-center">
+        port</div>
 )
 }
